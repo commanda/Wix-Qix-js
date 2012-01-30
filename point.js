5fee2e3015a0ea5594669e43ce13402bc8e3a82e
commanda <amanda.wixted@gmail.com>
2012-01-29 23:12:14 -0500

    added interpolation function to Point class

--- point.js
+++ point.js
@@ -1,8 +1,31 @@
-function Point(x, y){
+function Point(x, y)
+{
     this.x = x;
     this.y = y;
 }
 
-Point.prototype.toString = function() {
+Point.prototype.toString = function() 
+{
     return "(" + this.x + ", " + this.y + ")";
+}
+
+Point.prototype.floor = function() 
+{
+    this.x = Math.floor(this.x);
+    this.y = Math.floor(this.y);
+}
+
+var multiply = function(pointA, scalar)
+{
+    return new Point(pointA.x * scalar, pointB.y * scalar);
+}
+
+var add = function(pointA, pointB)
+{
+    return new Point(pointA.x + pointB.x, pointA.y + pointB.y);
+}
+
+var interpolate = function(pointA, pointB, percent) 
+{
+    return add(multiply(pointA, 1.0 - percent, multiply(pointB, percent));
 }
